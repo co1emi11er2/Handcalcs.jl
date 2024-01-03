adce953492ec3eea6d3a152f1e47f29761a82076
Documenter.jl <documenter@juliadocs.github.io>
2024-01-03 04:36:12 +0000

    build based on 99811e1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Handcalcs","category":"page"},{"location":"#Handcalcs","page":"Home","title":"Handcalcs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Handcalcs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Handcalcs]","category":"page"}]
+}
