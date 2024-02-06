0b7960f60e256c7d848a6a6896e249b37c27d6ac
co1emi11er2 <colemiller2010@gmail.com>
2024-02-06 14:51:23 -0600

    added Latexify kwarg tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,6 +14,22 @@ using Test
     @test calc2 == expected
 end
 
+@testset "Latexify kwargs" begin
+    # testing post kwarg
+    expected = L"$c = a + b = 1.234 + 5.678 = 7.0$"
+    a = 1.234
+    b = 5.678
+    calc = @handcalc c = a + b post = round
+    @test calc == expected
+
+    # testing fmt kwarg
+    expected = L"$c = a + b = 1.2340 + 5.6780 = 6.9120$"
+    a = 1.234
+    b = 5.678
+    calc = @handcalc c = a + b fmt = "%.4f"
+    @test calc == expected
+end
+
 @testset "UnitfulLatexify" begin
     # Simple test
     expected = L"$x = a + b = 2\;\mathrm{inch} -5\;\mathrm{inch} = -3\;\mathrm{inch}$"
