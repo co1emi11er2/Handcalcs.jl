04064c624385507f53e36a758e55f09feb08880b
co1emi11er2 <colemiller2010@gmail.com>
2024-02-06 18:01:31 -0600

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using Handcalcs
 using LaTeXStrings, Unitful, UnitfulLatexify
 using Test
 
-@testset "handcalc macro" begin
+@testset "handcalc macro    " begin
     # Quadratic formula test
     expected = L"$x = \frac{ - b + \sqrt{b^{2} - 4 \cdot a \cdot c}}{2 \cdot a} = \frac{ + 5 + \sqrt{\left( -5 \right)^{2} - 4 \cdot 2 \cdot 2}}{2 \cdot 2} = 2.0$"
     a = 2
@@ -14,7 +14,7 @@ using Test
     @test calc2 == expected
 end
 
-@testset "Latexify kwargs" begin
+@testset "Latexify kwargs   " begin
     # testing post kwarg
     expected = L"$c = a + b = 1.234 + 5.678 = 7.0$"
     a = 1.234
@@ -30,7 +30,7 @@ end
     @test calc == expected
 end
 
-@testset "UnitfulLatexify" begin
+@testset "UnitfulLatexify   " begin
     # Simple test
     expected = L"$x = a + b = 2\;\mathrm{inch} -5\;\mathrm{inch} = -3\;\mathrm{inch}$"
     a = 2u"inch"
@@ -46,13 +46,24 @@ end
     @test_broken calc == expected
 end
 
-@testset "handcalcs macro" begin
-    #TODO: Figure out way to write expected better and get it to pass
+@testset "handcalcs macro   " begin
+    # Single Expression - General
+    expected =  L"$\begin{align}
+x &= \frac{ - b + \sqrt{b^{2} - 4 \cdot a \cdot c}}{2 \cdot a} = \frac{ + 5 + \sqrt{\left( -5 \right)^{2} - 4 \cdot 2 \cdot 2}}{2 \cdot 2} = 2.0
+\end{align}$"
+    a = 2
+    b = -5
+    c = 2
+    calc = @handcalcs x = (-b + sqrt(b^2 - 4*a*c))/(2*a)
+    calc2 = @handcalcs begin x = (-b + sqrt(b^2 - 4*a*c))/(2*a) end
+    @test calc == expected
+    @test calc2 == expected    
+
+    # Multiple Expressions - General
     expected = (L"$\begin{align}
 Iy &= \frac{h \cdot b^{3}}{12} = \frac{10 \cdot 5^{3}}{12} = 104.16666666666667\text{  }(\text{moment of inertia y})
 \\Ix &= \frac{b \cdot h^{3}}{12} = \frac{5 \cdot 10^{3}}{12} = 416.6666666666667\text{  }(\text{moment of inertia x})
 \end{align}$")
-    # expected = (L"$\begin{align}\\Iy = \frac{h \cdot b^{3}}{12} = \frac{10 \cdot 5^{3}}{12} = 104.16666666666667\text{  }(\text{moment of inertia y})\\Ix = \frac{b \cdot h^{3}}{12} = \frac{5 \cdot 10^{3}}{12} = 416.6666666666667\text{  }(\text{moment of inertia x})\end{align}$")
     b = 5
     h = 10
     comment1 = "moment of inertia"
@@ -61,8 +72,61 @@ Iy &= \frac{h \cdot b^{3}}{12} = \frac{10 \cdot 5^{3}}{12} = 104.16666666666667\
 	    Ix = b*h^3/12; "moment of inertia x";
     end
     @test calc == expected
+
+    # Formatting check
     #TODO: Fix default to 4 decimals
     expected = (L"$\begin{align}\\Iy = \frac{h \cdot b^{3}}{12} = \frac{10 \cdot 5^{3}}{12} = 104.1667\text{  }(\text{moment of inertia y})\\Ix = \frac{b \cdot h^{3}}{12} = \frac{5 \cdot 10^{3}}{12} = 416.6667\text{  }(\text{moment of inertia x})\end{align}$")
     @test_broken calc == expected
 
+    # Array type - General and broadcasting
+    expected = (L"$\begin{align}
+z &= x + y = \left[
+\begin{array}{c}
+1 \\
+2 \\
+3 \\
+\end{array}
+\right] + \left[
+\begin{array}{c}
+4 \\
+5 \\
+6 \\
+\end{array}
+\right] = \left[
+\begin{array}{c}
+5 \\
+7 \\
+9 \\
+\end{array}
+\right]
+\end{align}$")
+
+    expected2 = (L"$\begin{align}
+z &= x \cdot y = \left[
+\begin{array}{c}
+1 \\
+2 \\
+3 \\
+\end{array}
+\right] \cdot \left[
+\begin{array}{c}
+4 \\
+5 \\
+6 \\
+\end{array}
+\right] = \left[
+\begin{array}{c}
+4 \\
+10 \\
+18 \\
+\end{array}
+\right]
+\end{align}$")
+    x = [1; 2; 3]
+    y = [4; 5; 6]
+    calc = @handcalcs z = x + y
+
+    calc2 = @handcalcs z = x .* y
+    @test calc == replace(expected, "\r" => "") # for whatever reason the expected had addittional carriage returns (\r) 
+    @test calc2 == replace(expected2, "\r" => "")
 end
