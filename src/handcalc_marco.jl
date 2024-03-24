85134dc44b91d4cb0d9883c656398995c2a9b48a
co1emi11er <colemiller2010@gmail.com>
2024-03-24 08:51:04 -0500

    Cleaned handfunc macro

--- src/handcalc_marco.jl
+++ src/handcalc_marco.jl
@@ -23,12 +23,12 @@ macro handcalc(expr, kwargs...)
     expr = unblock(expr)
 	expr = rmlines(expr)
     math_syms = [:*, :/, :^, :+, :-, :%, :.*, :./, :.^, :.+, :.-, :.%]
-    if @capture(expr, x_ = f_(fields__) | f_(fields__))
-        if f ∉ math_syms
-            return esc(:((@handfunc $expr)))
-        end
-        # return :($esc(@handfunc $expr))
-    end
+    # if @capture(expr, x_ = f_(fields__) | f_(fields__)) #future recursion
+    #     if f ∉ math_syms
+    #         return esc(:((@macroexpand @handfunc $expr)))
+    #     end
+    #     # return :($esc(@handfunc $expr))
+    # end
     
     expr_post = expr.head == :(=) ? expr.args[2:end] : expr
     expr_numeric = _walk_expr(expr_post, math_syms)
