6264f811b7beb909a99a9cf59954bb27aff6a923
co1emi11er <colemiller2010@gmail.com>
2024-03-24 09:28:14 -0500

    Added Examples

--- examples/hand_example.jl
+++ examples/hand_example.jl
@@ -4,24 +4,14 @@
 using Markdown
 using InteractiveUtils
 
-# ╔═╡ ab72ba8c-b356-11ee-0a39-0d1b38e5f4ca
-# ╠═╡ show_logs = false
-begin
-	using Revise, Pkg
-	Pkg.activate();
-end
-
 # ╔═╡ ce5b3335-daca-45cc-82bb-6a05ec02e9a5
-using Handcalcs, Latexify, LaTeXStrings, CodeTracking, BridgeCalcsUS
+using Handcalcs, Latexify, TestHandcalcFunctions
 
 # ╔═╡ 02f3158c-59a5-4282-88d4-35c4551c0033
-using Unitful, UnitfulLatexify, DataFrames
+using Unitful, UnitfulLatexify
 
 # ╔═╡ 59d2b9f7-1601-478a-99dc-e54f7166aafa
 using PlutoTeachingTools, PlutoUI; TableOfContents()
-
-# ╔═╡ af2113ee-0352-4b65-a269-723450ba54ea
-using CairoMakie
 
 # ╔═╡ 1bc35d4c-9391-4459-b961-d7b608324790
 const inch = u"inch";
@@ -81,75 +71,431 @@ end
 # ╔═╡ a156a574-e767-46a7-9885-27d698663135
 begin @handcalc d = (a + b) * c end
 
-# ╔═╡ bf091ac0-adce-42af-8be5-fe76fa70b3dc
-begin @latexdefine e = ($a + $b) * $c end
-
-# ╔═╡ de809996-e677-4c59-adcd-4697dc423965
-@latexdefine begin f = (-$b + sqrt($b^2 - 4*$a*$c))/(2*$a) end
-
-# ╔═╡ d35d01cd-7a80-4b3b-a64c-8dab2231d82b
-@handcalc begin Ix = calculate_Ix(b, h) end
-
-# ╔═╡ 2ce0e082-7235-4bb0-99ef-fd32b29782e6
-@handcalc begin IIx = b*h^3/12 end
-
 # ╔═╡ fb872702-2762-4542-9ac5-0b8809f5ebfd
 @handcalcs begin
-	IIy = h*b^3/12; "moment of inertia";
-	IIIx = b*h^3/12; "AASHTO 5.6.2";
+	Iy = h*b^3/12; "moment of inertia";
+	Ix = b*h^3/12; "AASHTO 5.6.2";
 end
 
 # ╔═╡ aa1131ee-fcf9-44b2-9d65-09bdeccb1e0c
-@handfunc begin Iy=calculate_Ix(b, h) end
+@handfunc begin I_x= TestHandcalcFunctions.calc_Ix(b, h) end
 
-# ╔═╡ defc22d4-4de6-4c6a-b60c-9ae937999c15
-md"### Table Example"
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+CodeTracking = "da1fd8a2-8d9e-5ec2-8556-3022fb5608a2"
+Handcalcs = "e8a07092-c156-4455-ab8e-ed8bc81edefb"
+LaTeXStrings = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
+Latexify = "23fbe1c1-3f47-55db-b15f-69d7ec21a316"
+PlutoTeachingTools = "661c6b06-c737-4d37-b85c-46df65de6f69"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+TestHandcalcFunctions = "6ba57fb7-81df-4b24-8e8e-a3885b6fcae7"
+Unitful = "1986cc42-f94f-5a68-af5c-568840ba703d"
+UnitfulLatexify = "45397f5d-5981-4c77-b2b3-fc36d6e9b728"
 
-# ╔═╡ 8d93b4ff-c972-47ad-b73c-a8691b32a442
-begin df = DataFrame(Dict(
-	"Height" => [2*inch, 4*inch, 6*inch], 
-	"Width" => [1*inch, 3*inch, 5*inch]
-)) 
-end
+[compat]
+CodeTracking = "~1.3.5"
+Handcalcs = "~0.1.5"
+LaTeXStrings = "~1.3.1"
+Latexify = "~0.16.2"
+PlutoTeachingTools = "~0.2.14"
+PlutoUI = "~0.7.58"
+TestHandcalcFunctions = "~0.1.0"
+Unitful = "~1.19.0"
+UnitfulLatexify = "~1.6.3"
+"""
 
-# ╔═╡ fe5a8486-87e1-46db-9ce4-adfca8b10a12
-md"### Plot Example"
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
 
-# ╔═╡ 4cc30f06-9b65-425a-a238-741b64a139c5
-y = -1000:50:1000;
+julia_version = "1.10.0"
+manifest_format = "2.0"
+project_hash = "808cdfe0958183556011e877e2aae6ebe88a2da1"
 
-# ╔═╡ b60b54c6-9a6f-4da5-9235-9e7c80c5f6af
-z = y.^3;
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "0f748c81756f2e5e6854298f11ad8b2dfae6911a"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.3.0"
 
-# ╔═╡ bde76f18-f5a5-4e05-8e8c-ac931c231743
-# begin
-# 	lines(y, z)
-# 	scatter!(y, z)
-# end
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
 
-# ╔═╡ 04f5fe71-fff8-4cc8-80c3-c978977eca05
-begin
-	fig, ax, l1 = lines(y, z)
-	s1 = scatter!(y, z)
-	ax.xlabel = "x label"
-	ax.ylabel = "y label"
-	ax.title = "Polynomial Plot"
-	fig
-end
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
 
-# ╔═╡ 998a6b3c-fb16-4f67-8666-a1112bd3b5e3
-arg = "AASHTO 5.6.2"
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
 
-# ╔═╡ 54c0b498-7a10-4baf-aa13-881407826b5c
+[[deps.CodeTracking]]
+deps = ["InteractiveUtils", "UUIDs"]
+git-tree-sha1 = "c0216e792f518b39b22212127d4a84dc31e4e386"
+uuid = "da1fd8a2-8d9e-5ec2-8556-3022fb5608a2"
+version = "1.3.5"
 
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "eb7f0f8307f71fac7c606984ea5fb2817275d6e4"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.4"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.0.5+1"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Distributed]]
+deps = ["Random", "Serialization", "Sockets"]
+uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "335bfdceacc84c5cdf16aadc768aa5ddfc5383cc"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.4"
+
+[[deps.Format]]
+git-tree-sha1 = "f3cf88025f6d03c194d73f5d13fee9004a108329"
+uuid = "1fa38f19-a742-5d3f-a2b9-30dd87b9d5f8"
+version = "1.3.6"
+
+[[deps.Handcalcs]]
+deps = ["CodeTracking", "InteractiveUtils", "LaTeXStrings", "Latexify", "MacroTools", "Revise"]
+git-tree-sha1 = "f0f41473d45396f2c0025236656a53d800a401fe"
+uuid = "e8a07092-c156-4455-ab8e-ed8bc81edefb"
+version = "0.1.5"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "179267cfa5e712760cd43dcae385d7ea90cc25a4"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.5"
+
+[[deps.HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.5"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "8b72179abc660bfab5e28472e019392b97d0985c"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.4"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.4"
+
+[[deps.JuliaInterpreter]]
+deps = ["CodeTracking", "InteractiveUtils", "Random", "UUIDs"]
+git-tree-sha1 = "0015726aa3eb28973fbbdbb7ca2ed6396ffe3a23"
+uuid = "aa1ae85d-cabe-5617-a682-6adf51b2e16a"
+version = "0.9.30"
+
+[[deps.LaTeXStrings]]
+git-tree-sha1 = "50901ebc375ed41dbf8058da26f9de442febbbec"
+uuid = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
+version = "1.3.1"
+
+[[deps.Latexify]]
+deps = ["Format", "InteractiveUtils", "LaTeXStrings", "MacroTools", "Markdown", "OrderedCollections", "Requires"]
+git-tree-sha1 = "cad560042a7cc108f5a4c24ea1431a9221f22c1b"
+uuid = "23fbe1c1-3f47-55db-b15f-69d7ec21a316"
+version = "0.16.2"
+
+    [deps.Latexify.extensions]
+    DataFramesExt = "DataFrames"
+    SymEngineExt = "SymEngine"
+
+    [deps.Latexify.weakdeps]
+    DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+    SymEngine = "123dc426-2d89-5057-bbad-38513e3affd8"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.4"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "8.4.0+0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibGit2_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
+uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
+version = "1.6.4+0"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.11.0+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.LoweredCodeUtils]]
+deps = ["JuliaInterpreter"]
+git-tree-sha1 = "31e27f0b0bf0df3e3e951bfcc43fe8c730a219f6"
+uuid = "6f1432cf-f94c-5a45-995e-cdbf5db27b0b"
+version = "2.4.5"
+
+[[deps.MIMEs]]
+git-tree-sha1 = "65f28ad4b594aebe22157d6fac869786a255b7eb"
+uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
+version = "0.1.4"
+
+[[deps.MacroTools]]
+deps = ["Markdown", "Random"]
+git-tree-sha1 = "2fa9ee3e63fd3a4f7a9a4f4744a52f4856de82df"
+uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
+version = "0.5.13"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.2+1"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2023.1.10"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.23+2"
+
+[[deps.OrderedCollections]]
+git-tree-sha1 = "dfdf5519f235516220579f949664f1bf44e741c5"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.6.3"
+
+[[deps.Parsers]]
+deps = ["Dates", "PrecompileTools", "UUIDs"]
+git-tree-sha1 = "8489905bcdbcfac64d1daa51ca07c0d8f0283821"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.8.1"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.10.0"
+
+[[deps.PlutoHooks]]
+deps = ["InteractiveUtils", "Markdown", "UUIDs"]
+git-tree-sha1 = "072cdf20c9b0507fdd977d7d246d90030609674b"
+uuid = "0ff47ea0-7a50-410d-8455-4348d5de0774"
+version = "0.0.5"
+
+[[deps.PlutoLinks]]
+deps = ["FileWatching", "InteractiveUtils", "Markdown", "PlutoHooks", "Revise", "UUIDs"]
+git-tree-sha1 = "8f5fa7056e6dcfb23ac5211de38e6c03f6367794"
+uuid = "0ff47ea0-7a50-410d-8455-4348d5de0420"
+version = "0.1.6"
+
+[[deps.PlutoTeachingTools]]
+deps = ["Downloads", "HypertextLiteral", "LaTeXStrings", "Latexify", "Markdown", "PlutoLinks", "PlutoUI", "Random"]
+git-tree-sha1 = "89f57f710cc121a7f32473791af3d6beefc59051"
+uuid = "661c6b06-c737-4d37-b85c-46df65de6f69"
+version = "0.2.14"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
+git-tree-sha1 = "71a22244e352aa8c5f0f2adde4150f62368a3f2e"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.58"
+
+[[deps.PrecompileTools]]
+deps = ["Preferences"]
+git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
+uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+version = "1.2.1"
+
+[[deps.Preferences]]
+deps = ["TOML"]
+git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
+uuid = "21216c6a-2e73-6563-6e65-726566657250"
+version = "1.4.3"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.Requires]]
+deps = ["UUIDs"]
+git-tree-sha1 = "838a3a4188e2ded87a4f9f184b4b0d78a1e91cb7"
+uuid = "ae029012-a4dd-5104-9daa-d747884805df"
+version = "1.3.0"
+
+[[deps.Revise]]
+deps = ["CodeTracking", "Distributed", "FileWatching", "JuliaInterpreter", "LibGit2", "LoweredCodeUtils", "OrderedCollections", "Pkg", "REPL", "Requires", "UUIDs", "Unicode"]
+git-tree-sha1 = "12aa2d7593df490c407a3bbd8b86b8b515017f3e"
+uuid = "295af30f-e4ad-537b-8983-00126c2a3abe"
+version = "3.5.14"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SparseArrays]]
+deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+version = "1.10.0"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.10.0"
+
+[[deps.SuiteSparse_jll]]
+deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
+uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
+version = "7.2.1+1"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.TestHandcalcFunctions]]
+git-tree-sha1 = "3df775c88fa815d10b9ca754e276256fa78cb41d"
+uuid = "6ba57fb7-81df-4b24-8e8e-a3885b6fcae7"
+version = "0.1.0"
+
+[[deps.Tricks]]
+git-tree-sha1 = "eae1bb484cd63b36999ee58be2de6c178105112f"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.8"
+
+[[deps.URIs]]
+git-tree-sha1 = "67db6cc7b3821e19ebe75791a9dd19c9b1188f2b"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.5.1"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Unitful]]
+deps = ["Dates", "LinearAlgebra", "Random"]
+git-tree-sha1 = "3c793be6df9dd77a0cf49d80984ef9ff996948fa"
+uuid = "1986cc42-f94f-5a68-af5c-568840ba703d"
+version = "1.19.0"
+
+    [deps.Unitful.extensions]
+    ConstructionBaseUnitfulExt = "ConstructionBase"
+    InverseFunctionsUnitfulExt = "InverseFunctions"
+
+    [deps.Unitful.weakdeps]
+    ConstructionBase = "187b0558-2788-49d3-abe0-74a17ed4e7c9"
+    InverseFunctions = "3587e190-3f89-42d0-90ee-14403ec27112"
+
+[[deps.UnitfulLatexify]]
+deps = ["LaTeXStrings", "Latexify", "Unitful"]
+git-tree-sha1 = "e2d817cc500e960fdbafcf988ac8436ba3208bfd"
+uuid = "45397f5d-5981-4c77-b2b3-fc36d6e9b728"
+version = "1.6.3"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.13+1"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.8.0+1"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.52.0+1"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+2"
+"""
 
 # ╔═╡ Cell order:
-# ╠═ab72ba8c-b356-11ee-0a39-0d1b38e5f4ca
 # ╠═ce5b3335-daca-45cc-82bb-6a05ec02e9a5
 # ╠═02f3158c-59a5-4282-88d4-35c4551c0033
 # ╠═59d2b9f7-1601-478a-99dc-e54f7166aafa
 # ╠═1bc35d4c-9391-4459-b961-d7b608324790
-# ╠═af2113ee-0352-4b65-a269-723450ba54ea
 # ╟─8de622aa-5a8a-4761-8f58-b651d1ae04b6
 # ╟─ff37c719-5e6e-44f6-8a7c-b15a4a04ffc5
 # ╠═2b3ad300-c287-47eb-a80c-3d6140ad13f3
@@ -158,22 +504,11 @@ arg = "AASHTO 5.6.2"
 # ╠═a14751cd-1ec4-4620-bc4b-e19abb50bb32
 # ╟─dc918ecf-23b1-49f0-8976-a3986d06e522
 # ╟─a7e0bd67-96f9-4d6d-895b-62be07deb9d1
-# ╟─7ade629c-5382-48b0-a70e-b5482f21a89d
+# ╠═7ade629c-5382-48b0-a70e-b5482f21a89d
 # ╟─f4c40265-adf0-4257-a953-73b2bee8974c
 # ╟─ef7ad156-0f24-4594-857e-df3635b06af6
-# ╟─a156a574-e767-46a7-9885-27d698663135
-# ╟─bf091ac0-adce-42af-8be5-fe76fa70b3dc
-# ╟─de809996-e677-4c59-adcd-4697dc423965
-# ╟─d35d01cd-7a80-4b3b-a64c-8dab2231d82b
-# ╟─2ce0e082-7235-4bb0-99ef-fd32b29782e6
-# ╟─fb872702-2762-4542-9ac5-0b8809f5ebfd
-# ╟─aa1131ee-fcf9-44b2-9d65-09bdeccb1e0c
-# ╟─defc22d4-4de6-4c6a-b60c-9ae937999c15
-# ╟─8d93b4ff-c972-47ad-b73c-a8691b32a442
-# ╟─fe5a8486-87e1-46db-9ce4-adfca8b10a12
-# ╠═4cc30f06-9b65-425a-a238-741b64a139c5
-# ╠═b60b54c6-9a6f-4da5-9235-9e7c80c5f6af
-# ╟─bde76f18-f5a5-4e05-8e8c-ac931c231743
-# ╟─04f5fe71-fff8-4cc8-80c3-c978977eca05
-# ╠═998a6b3c-fb16-4f67-8666-a1112bd3b5e3
-# ╠═54c0b498-7a10-4baf-aa13-881407826b5c
+# ╠═a156a574-e767-46a7-9885-27d698663135
+# ╠═fb872702-2762-4542-9ac5-0b8809f5ebfd
+# ╠═aa1131ee-fcf9-44b2-9d65-09bdeccb1e0c
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
